47afbcd534bcc02b4da7ad7821e0af609afa6696
casscannelle <cassia.maffei@gmail.com>
2024-02-07 12:03:40 -0300

    ajuste na questão com radio button

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
-import GenderFormItem from '../Radio/radio';
 
 function Form() {
-  const [inputValue, setInputValue] = useState({ nome: '', email: '', idade: ''});
-  const [gender, setGender] = useState('');
+  const [inputValue, setInputValue] = useState({ nome: '', email: '', idade: '', gender: 'Feminino'});
   const [showAnswers, setshowAnswers] = useState(false);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setInputValue((prevInputValue) => ({ ...prevInputValue, [name]: value }));
+    setshowAnswers(false);
   };
 
   const handleSubmit = (event) => {
@@ -21,23 +20,26 @@ function Form() {
     setshowAnswers(false);
   }
 
-  const handleGenderChange = (selectedGender) => {
-    setGender(selectedGender);
-  };
-
-
   return (
     <>
     <div className='container-form'>
       <form>
         <h1 className='form_h1'>Preencha seus dados</h1>
         <label>Nome:</label>
-        <input className='form_input' type="text" name="nome" value={inputValue.nome} onChange={handleChange} minlength="3" required={true} />
+        <input className='form_input' type="text" name="nome" value={inputValue.nome} onChange={handleChange} minlength="3" />
         <label>Idade:</label>
-        <input className='form_input' type="number" name="idade" value={inputValue.idade} onChange={handleChange} min="0" required={true} />
+        <input className='form_input' type="number" name="idade" value={inputValue.idade} onChange={handleChange} min="0" />
         <label>E-mail:</label>
-        <input className='form_input' type="email" name="email" value={inputValue.email} onChange={handleChange} required={true} />
-        <GenderFormItem selectedOption={gender} onOptionChange={handleGenderChange} />
+        <input className='form_input' type="email" name="email" value={inputValue.email} onChange={handleChange} />
+        <label>Gênero</label>
+        <div>
+          <input type='radio' name="gender" value="Feminino" id="feminino" checked={inputValue.gender === "Feminino"} onChange={handleChange} />
+          <label htmlFor="feminino">Feminino</label>
+        </div>
+        <div>
+          <input type='radio' name="gender" value="Masculino" id="masculino" checked={inputValue.gender === "Masculino"} onChange={handleChange} />
+          <label htmlFor="masculino">Masculino</label>
+        </div>
         <button className='btn' type="submit" onClick={handleSubmit}>Enviar</button>
       </form>
       </div>
@@ -56,7 +58,7 @@ function Form() {
             <h3>E-mail:</h3> {inputValue.email}
           </p>
           <p>
-            <h3>Gênero:</h3> {gender}
+            <h3>Gênero:</h3> {inputValue.gender}
           </p>
           <button className='btn' onClick={hideAnswers}>Limpar</button>
         </div>
